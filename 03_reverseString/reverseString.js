cc876c085cb986309613da9faf1e20a252cebde0
Faris <125892193+XxFARISxX@users.noreply.github.com>
2024-04-05 23:11:10 +0300

    nothing to say here.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,8 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    
+    let stringArr = input.split("").reverse().join("");
+
+    return stringArr;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...args) {
+
+    
 
 };
 
