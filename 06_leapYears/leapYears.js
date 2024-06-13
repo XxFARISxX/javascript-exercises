fc435402bf21c1a75cd57284bffd2d5590894727
Faris <125892193+XxFARISxX@users.noreply.github.com>
2024-06-14 02:09:40 +0300

    finished leapYears and tempConversion exercise.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    
+   let division = [years % 4, years % 400, years % 100];
+   if ((division[2] !== 0) && (division[0] === 0) || (division[1] === 0)) {
+    return true
+   } else {return false};
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+
+  let conversion = (F - 32) * 5 / 9;
+
+  return parseFloat(conversion.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+
+  let conversion = C * 9 / 5 + 32;
+
+  return parseFloat(conversion.toFixed(1));
+
 };
 
 // Do not edit below this line
